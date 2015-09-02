e94cfa4fea274f728f601d2876b3b0b379e197fb
Eric Brelsford <ebrelsford@gmail.com>
2015-09-02 10:57:37 -0400

    Latest build

--- build/dist.sh
+++ build/dist.sh
@@ -3,17 +3,17 @@
 # Build and push to gh-pages
 START_BRANCH=`git rev-parse --abbrev-ref HEAD`
 PUBLISH_BRANCH=gh-pages
-DEST_DIR=sidewalk-temp
+DEST_DIR=sidewalk-zombie-temp
 
 npm install
 gulp build
 cd ..
 rm -rf $DEST_DIR
-git clone git@github.com:curbyourlitter/curbyourlitter-sidewalk.git $DEST_DIR
+git clone git@github.com:curbyourlitter/curbyourlitter-sidewalk-zombie.git $DEST_DIR
 cd $DEST_DIR
 git checkout $PUBLISH_BRANCH
 rm -rf *
-cp -r ../curbyourlitter-sidewalk/dist/* .
+cp -r ../curbyourlitter-sidewalk-zombie/dist/* .
 git add -A
 git commit -am "Latest build"
 git push origin $PUBLISH_BRANCH
