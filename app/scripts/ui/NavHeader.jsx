c2fbfb622acd07fcb04343783f9af9620c28007d
Eric Brelsford <ebrelsford@gmail.com>
2015-10-14 16:24:07 -0400

    Nav to home

--- app/scripts/ui/NavHeader.jsx
+++ app/scripts/ui/NavHeader.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Nav, NavBrand, Navbar, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 export var NavHeader = React.createClass({
     render: function () {
         return (
             <header className="nav-header">
-                <div className="navbar-brand">
+                <Link to="/" className="navbar-brand">
                     <div className="logo"></div>
                     <div className="brand-text">
                         <div className="brand">Curb Your Litter</div>
                         <div className="city">Greenpoint</div>
                     </div>
-                </div>
+                </Link>
                 <Navbar fixedTop toggleNavKey={0}>
                     <Nav eventKey={0}>
                         <NavItem href="http://curbyourlitter.org/the-problem/">The Problem</NavItem>
                         <NavItem href="http://curbyourlitter.org/what-can-we-do/">What Can We Do?</NavItem>
                         <NavItem href="http://curbyourlitter.org/news-events/">News &amp; Events</NavItem>
                         <NavItem href="http://curbyourlitter.org/about/">About</NavItem>
-                        <NavItem>Map the Trash!</NavItem>
+                        <NavItem href="/">Map the Trash!</NavItem>
                     </Nav>
                 </Navbar>
             </header>
